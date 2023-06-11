c1770b526e70e266b91d0112249104ddd417bb1d
Voloshyn Pavlo <v4voloshyn@gmail.com>
2023-06-12 01:39:54 +0300

    feat: setup test environment

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
+/// <reference types="vite/client" />
+/// <reference types="vitest" />
+
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/tests/setupTests.ts'],
+    includeSource: ['src/**/*.{ts,tsx}'],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      reportsDirectory: './src/tests/unit/coverage',
+    },
+  },
   plugins: [react()],
   server: {
     port: 5555,
